4e3452d9b963ed5cd1be5aadca16d2b398d6a441
jijysun <jijysun@naver.com>
2025-03-30 19:29:34 +0900

    test commit

---
+++ src/main/java/Hello_OAuth2/OAuth2_Study/OAuth2StudyApplication.java
@@ -0,0 +1,13 @@
+package Hello_OAuth2.OAuth2_Study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2StudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hello_OAuth2/OAuth2_Study/OAuth2StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package Hello_OAuth2.OAuth2_Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
